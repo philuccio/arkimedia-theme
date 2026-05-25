72fd91319194242a17479fa8d1fa13216782312c
Philo Philo <philuccio@MBP-di-Philo.home-life.hub>
2026-05-25 23:42:18 +0200

    fix: rimosso duplicato align-items nel pannello Layout

--- blocks/row/build/index.asset.php
+++ blocks/row/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '655073b9c30f5bd2d9ab');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '44e9f64fab8fa3466744');
